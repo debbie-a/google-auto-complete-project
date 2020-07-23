36e70db390ba776b840c8ad9e857bef1b61ba9f2
debbie-a <debbiealter100@gmail.com>
2020-07-23 17:26:26 +0300

    start searching........

--- search_engine.py
+++ search_engine.py
@@ -1,20 +1,27 @@
+import linecache
 import os
+import string
 from collections import defaultdict
 import re
 from string import ascii_lowercase, digits
 
 from auto_complete_data import AutoCompleteData
 
+K = 5
+LENGTH_LIMIT = 10
+
 
 def clean_string(string_):
-    pattern = re.compile('[^A-Za-z0-9 -]')
-    string_ = pattern.sub('', string_)
-    return " ".join(string_.split()).lower()
+    string_ = string_.translate(string_.maketrans("", "", string.punctuation))
+
+    return re.sub(' +', ' ', string_).lower().strip()
 
 
 def get_files(file_dict):
+    # root
     path = f'{AutoCompleteData.root}'
 
+    # add every path to hash map
     for root, dirs, files in os.walk(path):
         for i, file in enumerate(files):
             file_dict[i] = os.path.join(root, file)
@@ -24,14 +31,15 @@ def read_data_from_file(file):
     my_file = open(file, encoding="utf8")
     data = my_file.read().split("\n")
     my_file.close()
+
     return data
 
 
 def load_data_from_files(list_, file_dict, prefix):
-    # load autocomplete objects into dict from file data
     data = {}
     for pair in list_:
-        sentence = read_data_from_file(file_dict[pair[0]])[pair[1] - 1]
+        # load autocomplete objects into dict from file data
+        sentence = linecache.getline(file_dict[pair[0]], pair[1])[:-1]
         data['*'.join(map(str, pair))] = AutoCompleteData(sentence, file_dict[pair[0]][:file_dict[pair[0]].index(".")],
                                                           pair[1], AutoCompleteData.get_score(prefix, sentence))
     return data
@@ -45,6 +53,7 @@ def who_to_remove(list_):
 def remove_lowest_score(list_, file_dict, prefix):
     data = load_data_from_files(list_, file_dict, prefix)
     tmp_list = []
+
     for val in data.values():
         tmp_list.append(val)
 
@@ -52,33 +61,45 @@ def remove_lowest_score(list_, file_dict, prefix):
     pair_to_remove = list(data.keys())[list(data.values()).index(min_score)]
     list_.remove(list(map(int, pair_to_remove.split("*"))))
 
+    del tmp_list
+
 
 def remove_duplicate_lists_from_list(list_to_remove_duplicates):
     tmp = []
     for list_ in list_to_remove_duplicates:
         if list_ not in tmp:
             tmp.append(list_)
+
+    del list_to_remove_duplicates
     return tmp
+
+
+def get_all_string_sub_strings(sentence_):
+    return [sentence_[i: j] for i in range(min(len(sentence_), LENGTH_LIMIT)) for j in range(i + 1, len(sentence_) + 1)]
 
 
 def init_data_collection(data_collection, file_dict):
     print("Loading the files and preparing the system...")
+
+    # get all paths of files from where we need to store data from and init file hash map
     get_files(file_dict)
 
+    # for each file read data
     for key_, file in file_dict.items():
         file_id = key_
         data = read_data_from_file(file)
 
+        # taking care of each line of data
         for file_line, sentence in enumerate(data, 1):
             sentence_ = clean_string(sentence[::])
 
-            for key in [sentence_[i: j] for i in range(len(sentence_)) for j in range(i + 1, len(sentence_) + 1)]:
+            for substring in get_all_string_sub_strings(sentence_):
+                data_collection[substring] += [[file_id, file_line]]
+                data_collection[substring] = remove_duplicate_lists_from_list(data_collection[substring])
 
-                data_collection[key] += [[file_id, file_line]]
-                data_collection[key] = remove_duplicate_lists_from_list(data_collection[key])
-
-                if len(data_collection[key]) > 5:
-                    remove_lowest_score(data_collection[key], file_dict, key)
+                # if a substring has more than k matches, take care...
+                if len(data_collection[substring]) > K:
+                    remove_lowest_score(data_collection[substring], file_dict, substring)
 
 
 def remove_duplicate_objects_from_list(list_):
@@ -89,60 +110,92 @@ def remove_duplicate_objects_from_list(list_):
                     list_.remove(min(object_1, object_2, key=lambda x: x.score))
 
 
-def complete_word(prefix, data_collection, file_dict):
-    list_ = []
+def add_letter_to_match(data_collection, file_dict, prefix, i):
+    SCORING = [5, 4, 3, 2, 1]
+    tmp = get_best_k_completions(data_collection, file_dict, prefix)
+    remove_duplicate_objects_from_list(tmp)
+    for object_ in tmp:
+        object_.set_score(SCORING[i] if i < len(SCORING) else SCORING[-1])
+    return tmp
+
+
+def remove_letter_to_match(data_collection, file_dict, prefix, i):
+    SCORING = [10, 8, 6, 4, 2]
+    tmp = get_best_k_completions(data_collection, file_dict, prefix)
+    remove_duplicate_objects_from_list(tmp)
+    for object_ in tmp:
+        object_.set_score(SCORING[i] if i < len(SCORING) else SCORING[-1])
+    return tmp
+
+
+def change_letter_to_match(data_collection, file_dict, prefix, i):
+    SCORING = [5, 4, 3, 2, 1]
+
+    tmp = get_best_k_completions(data_collection, file_dict, prefix)
+    remove_duplicate_objects_from_list(tmp)
+    for object_ in tmp:
+        object_.set_score(SCORING[i] if i < len(SCORING) else SCORING[-1])
+    return tmp
+
+
+def complete_word(data_collection, file_dict, prefix, search_result):
     CHAR_LIST = list(ascii_lowercase) + list(digits)
 
     for i in range(len(prefix)):
 
         # remove one letter to match a completion
         if prefix[:i] + prefix[i + 1:] in list(data_collection.keys()):
-            tmp = get_best_k_completions(prefix[:i] + prefix[i + 1:], data_collection, file_dict)
-            remove_duplicate_objects_from_list(tmp)
-            for object_ in tmp:
-                object_.set_score(10 - i * 2 if i < 4 else 2)
+            search_result += remove_letter_to_match(data_collection, file_dict, prefix[:i] + prefix[i + 1:], i)
 
-            list_ += tmp
-            while len(list_) > 5:
-                list_.remove(who_to_remove(list_))
+            while len(search_result) > K:
+                search_result.remove(who_to_remove(search_result))
 
         for char_ in CHAR_LIST:
 
             # change one letter to match a completion
             if prefix[:i] + char_ + prefix[i + 1:] in list(data_collection.keys()):
-                tmp = get_best_k_completions(prefix[:i] + prefix[i + 1:], data_collection, file_dict)
-                remove_duplicate_objects_from_list(tmp)
-                for object_ in tmp:
-                    object_.set_score(5 - i if i < 5 else 1)
+                search_result += change_letter_to_match(data_collection, file_dict, prefix[:i] + prefix[i + 1:], i)
 
-                list_ += tmp
-                while len(list_) > 5:
-                    list_.remove(who_to_remove(list_))
+                while len(search_result) > K:
+                    search_result.remove(who_to_remove(search_result))
 
             # add one letter to match a completion
             if prefix[:i] + char_ + prefix[i:] in list(data_collection.keys()):
-                tmp = get_best_k_completions(prefix[:i] + char_ + prefix[i:], data_collection, file_dict)
-                remove_duplicate_objects_from_list(tmp)
-                for object_ in tmp:
-                    object_.set_score(5 - i if i < 5 else 1)
+                search_result += add_letter_to_match(data_collection, file_dict, prefix[:i] + char_ + prefix[i:], i)
 
-                list_ += tmp
-
-                while len(list_) > 5:
-                    list_.remove(who_to_remove(list_))
+                while len(search_result) > K:
+                    search_result.remove(who_to_remove(search_result))
 
     # just in case...
-    while len(list_) > 5:
-        list_.remove(who_to_remove(list_))
+    while len(search_result) > K:
+        search_result.remove(who_to_remove(search_result))
 
-    remove_duplicate_objects_from_list(list_)
+    remove_duplicate_objects_from_list(search_result)
 
-    return list_
+    return search_result
 
 
-def get_best_k_completions(prefix, data_collection, file_dict):
-    auto_completes = load_data_from_files(data_collection[prefix], file_dict, prefix)
-    return list(auto_completes.values())
+def get_best_k_completions(data_collection, file_dict, prefix):
+    list_of_auto_completes = load_data_from_files(data_collection[prefix], file_dict, prefix)
+    return list(list_of_auto_completes.values())
+
+
+def offline(data_collection, file_dict):
+    # initialize the data structure (hash map)
+    init_data_collection(data_collection, file_dict)
+
+
+def online(data_collection, file_dict, string_to_complete):
+    # get best k completions
+    search_result= get_best_k_completions(data_collection, file_dict, clean_string(string_to_complete))
+
+    # found - return to user...
+    if len(search_result) == K:
+        return search_result
+
+    # try to find similar completions to match search
+    else:
+        return complete_word(data_collection, file_dict, string_to_complete, search_result)
 
 
 def read_input_from_user(message):
@@ -150,19 +203,21 @@ def read_input_from_user(message):
 
 
 def run():
-    STOP_INPUT = '#'
+
     data_collection = defaultdict(list)
     file_dict = {}
-    init_data_collection(data_collection, file_dict)
+
+    # offline
+    offline(data_collection, file_dict)
+
+    # online
+    STOP_INPUT = '#'
     while True:
         string_to_complete = read_input_from_user("The system is ready. Enter your text:\n")
 
         while string_to_complete[len(string_to_complete) - 1] != STOP_INPUT:
 
-            search_result = get_best_k_completions(clean_string(string_to_complete), data_collection, file_dict)
-
-            if not search_result:
-                search_result = complete_word(clean_string(string_to_complete), data_collection, file_dict)
+            search_result = online(data_collection, file_dict, string_to_complete)
 
             if search_result:
                 print(f'Here are {len(search_result)} suggestions') if len(search_result) > 1 else print("Here is 1 "
