a32e1a3c86d0d2f3c2596594593a33383ca3723a
debbie-a <debbiealter100@gmail.com>
2020-07-24 13:25:55 +0300

    all data is saved in json files to save on memory... small problem with scoring......

--- auto_complete_data.py
+++ auto_complete_data.py
@@ -1,7 +1,7 @@
 
 
 class AutoCompleteData:
-    root = "technology_texts\\"
+    root = "file\\"
 
     def __init__(self, sentence, source, offset, score_):
         self.completed_sentence = sentence
@@ -10,13 +10,13 @@ class AutoCompleteData:
         self.score = score_
 
     def __str__(self):
-        return f'{self.completed_sentence} ({self.source_text} {self.offset})'
+        return f'{self.completed_sentence} ({self.source_text} {self.offset}) score {self.score}'
 
     def set_score(self, points_to_decrement):
         self.score -= points_to_decrement
 
     @staticmethod
-    def get_score(word, sentence):
+    def get_score(word):
         return len(word) * 2
 
 
